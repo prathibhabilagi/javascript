94b8805969684c3cfee1da20dd6965525eecaaec
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-03-18 11:14:09 +0530

    Handle Async functions making use of promise, async and await

---
+++ ES6/Promise.js
@@ -0,0 +1,29 @@
+const uno = () => {
+   return "One";
+}
+
+const dos = () => {
+   return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Two");
+        }, 3000);
+    })
+   
+}
+
+const tres = () => {
+    return "Three";
+}
+
+const callMe = async() => {
+    let valOne = uno();
+    console.log(valOne);
+
+    let valTwo = await dos();
+    console.log(valTwo);
+
+    let valThree = tres();
+    console.log(valThree);
+}
+
+callMe();
